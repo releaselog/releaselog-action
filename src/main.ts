d0a9c7c32d9380356a4dab4599fdd627fd85b699
Kazuki Nishiura <kazuki@releaselog.app>
2021-03-18 17:30:15 -0700

    Add prettier

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
-import * as core from '@actions/core';
-import * as YAML from 'yaml';
+import * as core from "@actions/core";
+import * as YAML from "yaml";
 import { existsSync, readFileSync } from "fs";
 import * as path from "path";
 
 async function main(): Promise<void> {
   try {
-    const apiKey = core.getInput('api_key');
+    const apiKey = core.getInput("api_key");
     if (!apiKey) {
       core.setFailed("please specify releaselog API key");
       return;
     }
 
-    const configPath = path.join(process.env.GITHUB_WORKSPACE!, ".releaselog/config.yml");
+    const configPath = path.join(
+      process.env.GITHUB_WORKSPACE!,
+      ".releaselog/config.yml"
+    );
     if (existsSync(configPath)) {
       // config file exists
-      const config = YAML.parse(readFileSync(configPath, {encoding: 'utf8'}));
+      const config = YAML.parse(readFileSync(configPath, { encoding: "utf8" }));
       const pathMap = config["path_mappings"];
       console.log(config);
       console.log(pathMap);
